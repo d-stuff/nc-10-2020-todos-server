c175693a9fdc28ee0facca55fe3c7778f754e579
David Meir-Levy <davidmeirlevy@gmail.com>
2021-01-20 21:02:50 +0200

    todos model

--- todo-model.js
+++ todo-model.js
@@ -1,1 +1,22 @@
 require('./file-db')
+const { getData, setData } = require('./file-db');
+
+const TODOS_PATH = 'todos.json';
+
+async function addTodo(todo) {
+	const todos = await getData(TODOS_PATH);
+	todos.push(todo);
+	await setData(TODOS_PATH, todos);
+}
+
+async function removeTodo(todoId) {
+	const todos = await getData(TODOS_PATH);
+	const filteredTodos = todos.filter(todo => todo.id !== todoId);
+	await setData(TODOS_PATH, filteredTodos);
+}
+
+
+module.exports = {
+	addTodo,
+	removeTodo
+}
--- todos-cmd.js
+++ todos-cmd.js
@@ -1,7 +1,5 @@
 require('./todo-model')
 
 module.exports = function runCommand([ a, b, action, value ]) {
-
 	console.log(action, value);
-
 }
