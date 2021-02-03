2c81d01f14c0cf1747f2cf074962c41e019eb885
David Meir-Levy <davidmeirlevy@gmail.com>
2021-02-03 18:30:27 +0200

    todos endpoints

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json();
 const service = require('./todos-service');
 
 const app = express();
+
+// middleware example:
+app.use((req, res, next) => {
+	//
+	next();
+});
 
 app.get('/api/todos', async (req, res) => {
 	const filters = {};
@@ -19,7 +28,17 @@ app.get('/api/todos', async (req, res) => {
 app.delete('/api/todos/:todoId', async (req, res) => {
 	await service.removeTodo(Number(req.params.todoId));
 	res.json({ message: 'Todo removed successfully' });
-})
+});
+
+app.post('/api/todos', jsonParser, async (req, res) => {
+	const newTodo = await service.addTodo(req.body);
+	res.json(newTodo);
+});
+
+app.put('/api/todos/:todoId', jsonParser, async (req, res) => {
+	const updatedTodo = await service.updateTodo(Number(req.params.todoId), req.body);
+	res.json(updatedTodo);
+});
 
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
